307a03f53b6d108f3176434e2801275458674400
Shreyan <shreyan010@gmail.com>
2024-09-28 00:21:27 +0530

    push-7

--- src/walletConnect/siwe.ts
+++ src/walletConnect/siwe.ts
@@ -20,7 +20,7 @@ import {
     name: 'AgentsGuild',
     description: 'AgentsGuild is a multi-LLM project built with langgraph that leverages specialized models and Gaianet nodes to solve complex tasks like code generation, integration, and DeFi research with efficiency and precision.',
     url: 'https://agentsguild.vercel.app/', // origin must match your domain & subdomain
-    icons: ['https://i.postimg.cc/zBX50qgG/sw-apconnect.png'],
+    icons: ['https://raw.githubusercontent.com/shreyan001/AgentsGuild/refs/heads/main/public/guildlogo.png'],
   };
   
   export const publicClient = createPublicClient({
